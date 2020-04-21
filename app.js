4eeba7d222b879254eef8dbaa9fe06af87435d5c
clockwerkz <clockwerkz@gmail.com>
2020-04-20 22:52:21 -0700

    finished up game logic and game loop

--- app.js
+++ app.js
@@ -1,49 +1,71 @@
 const cvs = document.getElementById("canvas");
 const ctx = cvs.getContext('2d');
 
-let px = 180;
-let py = -20;
-offset = 300;
-let bx = 30;
-let by = 300;
 
 let bg = new Image();
-bg.src = "images/bg.png";
-bg.onload = function() {
-    ctx.drawImage(this, 0, 0);
-}
-
 let fg = new Image();
-fg.src = "images/fg.png";
-fg.onload = function() {
-    ctx.drawImage(this, 0, cvs.height - fg.height);
-}
-
 let bird = new Image();
-bird.src = "images/bird.png";
-bird.onload = function() {
-    ctx.drawImage(this, bx, by);
-}
-
 let pipeNorth = new Image();
+let pipeSouth = new Image();
+
+bg.src = "images/bg.png";
+fg.src = "images/fg.png";
+bird.src = "images/bird.png";
 pipeNorth.src = "images/pipeNorth.png";
-pipeNorth.onload = function() {
-    ctx.drawImage(this, px, py)
+pipeSouth.src = "images/pipeSouth.png";
+
+let gap = 75;
+let constant = pipeNorth.height + gap;
+let pipe = [];
+let px = 180;
+let py = -20;
+let bx = 30;
+let by = 300;
+let gravity = 1.5;
+let score = 0;
+pipe[0] = {
+    x:cvs.width,
+    y:0
 }
 
-let pipeSouth = new Image();
-pipeSouth.src = "images/pipeSouth.png";
-pipeSouth.onload = function() {
-    ctx.drawImage(this, px, py + offset)
-}
+document.addEventListener("keydown", (e)=> {
+
+    if (e.keyCode === 32) {
+        by-=25 ; 
+    }
+})
 
 ctx.drawImage(bg,0,0);
 function draw() {
     ctx.drawImage(bg, 0, 0);
-    ctx.drawImage(pipeNorth, px, py);
-    ctx.drawImage(pipeSouth, px, py+offset);
+    
+    for (let i=0; i<pipe.length; i++) {
+        ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
+        ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y+constant);
+        pipe[i].x--;
+        if (pipe[i].x === cvs.width / 2) {
+            pipe.push( {
+                x:cvs.width,
+                y:Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
+            });
+        }
+        if (bx + bird.width >= pipe[i].x && bx <= pipe[i].x + pipeNorth.width && 
+            (by <= pipe[i].y + pipeNorth.height || by+bird.height >= pipe[i].y+constant) 
+            || by + bird.height >= cvs.height - fg.height) {
+            location.reload();
+        }
+        if (pipe[i].x === 5) {
+            score++;
+        }
+    }
     ctx.drawImage(fg, 0, cvs.height - fg.height);
     ctx.drawImage(bird, bx, by);
+
+    by+=gravity;
+    ctx.fillStyle = "#000";
+    ctx.font = "20px Verdana";
+    ctx.fillText("Score : "+score, 10, cvs.height - 20);
+    requestAnimationFrame(draw);
 }
 
-requestAnimationFrame(draw);
+draw();
